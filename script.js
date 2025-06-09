cb18c653a074a9eb8608df9520a005c6a78a7c32
Shan98K <harsh98kmailbox@gmail.com>
2025-06-09 19:11:24 +0530

    ThirdFull commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// To get from Intro Ending to the next main page
+
 window.addEventListener("DOMContentLoaded", () => {
     const intro = document.querySelector(".bop");
 
@@ -7,4 +9,16 @@ window.addEventListener("DOMContentLoaded", () => {
             window.location.href = "nextpage.html";
         }
     });
+});
+
+// Scroll Effect from .navopts to .page{1,2,3}
+
+document.querySelector('.top').addEventListener('click', () => {
+    document.getElementById('page1').scrollIntoView({ behavior: 'smooth' });
+});
+document.querySelector('.about').addEventListener('click', () => {
+    document.getElementById('page2').scrollIntoView({ behavior: 'smooth' });
+});
+document.querySelector('.details').addEventListener('click', () => {
+    document.getElementById('page3').scrollIntoView({ behavior: 'smooth' });
 });
